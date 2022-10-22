46314f19c82495bbe3d325f07b6f01089001514c
ristafn <golemmite23@gmail.com>
2022-10-22 21:28:12 +0700

    Add assets

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,7 +1,10 @@
 class AppAssets {
   // Images
   static const String greetingImage = 'assets/images/greeting-image.png';
+  static const String hamburgerImage = 'assets/images/hamburger.png';
 
   // Icons
   static const String googleIcon = 'assets/icons/google-icon.png';
+  static const String cartIcon = 'assets/icons/cart.png';
+  static const String searchIcon = 'assets/icons/search.png';
 }
